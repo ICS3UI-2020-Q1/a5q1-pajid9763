9fef420c69e1c4891131fc913fb3731ff7e6c6e4
pajid9763 <71140779+pajid9763@users.noreply.github.com>
2020-10-06 10:50:58 -0400

    finished 1-12 multiplication table

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * program for a 1-12 multiplication table
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +11,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  Scanner input = new Scanner(System.in);
+
+  // ask the user for an integer
+  System.out.println("Please enter an integer to create a multiplication table for");
+  int num = input.nextInt();
+
+  // create the multiplication table
+  for (int i = 1; i <= 12; i++){
+    int sum = i * num;
+    System.out.println(i + " x " + num + " = " + sum);
+  }
     
   }
 }
